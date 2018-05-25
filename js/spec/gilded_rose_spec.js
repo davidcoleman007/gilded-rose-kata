4f147c2d965be2b55265e4eb7a5ecb3a297de221
David Coleman <david_coleman_007@hotmail.com>
2018-05-25 16:53:31 -0400

    refactored update_quality function added "conjured" items to update_quality function wrote all tests for each and every use case given need to speak with goblin regarding category keys

--- js/spec/gilded_rose_spec.js
+++ js/spec/gilded_rose_spec.js
@@ -1,9 +1,151 @@
-describe("Gilded Rose", function() {
+describe("Gilded Rose -> updateQuality()", function() {
 
-  it("should foo", function() {
-    items = [ new Item("foo", 0, 0) ];
-    update_quality();
-    expect(items[0].name).toEqual("fixme");
+  it("is a function", function() {
+    expect(typeof(update_quality)).toBe('function');
   });
 
+  it('Once the sell by date has passed, quality degrades twice as fast', () => {
+    items = [
+      new Item('foo', -1, 4)
+    ];
+    update_quality();
+    expect(items[0].quality).toBe(2);
+  });
+
+  it('The quality of an item is never negative', () => {
+    items = [
+      new Item('foo', 3, 4)
+    ];
+    update_quality();
+    update_quality();
+    update_quality();
+    update_quality();
+    update_quality();
+    update_quality();
+    update_quality();
+    expect(items[0].quality).toBe(0);
+  });
+
+  it('The quality of an item is never more than 50', () => {
+    items = [
+      new Item(AGED_BRIE, 3, 48)
+    ];
+    update_quality();
+    update_quality();
+    update_quality();
+    update_quality();
+    update_quality();
+    update_quality();
+    update_quality();
+    expect(items[0].quality).toBe(50);
+  });
+
+  it('"Aged Brie" actually increases in quality the older it gets', () => {
+    items = [
+      new Item(AGED_BRIE, 3, 4)
+    ];
+    update_quality();
+    update_quality();
+    update_quality();
+    update_quality();
+    update_quality();
+    update_quality();
+    update_quality();
+    expect(items[0].quality).toBe(11);
+  });
+
+  it('"Backstage Passes" increase in quality as sell_in date approaches', () => {
+    items = [
+      new Item(TAFKAL_CONCERT_BACKSTAGE, 20, 5)
+    ];
+    update_quality();
+    update_quality();
+    update_quality();
+    update_quality();
+    update_quality();
+    expect(items[0].quality).toBe(10);
+  });
+
+  it('"Backstage Passes" increase in quality by 2 when sell_in date is <= 10', () => {
+    items = [
+      new Item(TAFKAL_CONCERT_BACKSTAGE, 10, 10)
+    ];
+    update_quality(); // 10 + 2
+    update_quality(); // 12 + 2
+    update_quality(); // 14 + 2
+    update_quality(); // 16 + 2
+    update_quality(); // 18 + 2
+    expect(items[0].quality).toBe(20);
+  });
+
+  it('"Backstage Passes" increase in quality by 3 when sell_in date is <= 5', () => {
+    items = [
+      new Item(TAFKAL_CONCERT_BACKSTAGE, 5, 20)
+    ];
+    update_quality(); // + 3 = 23
+    update_quality(); // + 3 = 26
+    update_quality(); // + 3 = 29
+    update_quality(); // + 3 = 32
+    update_quality(); // + 3 = 35
+    expect(items[0].quality).toBe(35);
+  });
+
+  it('"Backstage Passes" have no value after the concert', () => {
+    items = [
+      new Item(TAFKAL_CONCERT_BACKSTAGE, 5, 5)
+    ];
+    update_quality(); // + 3 = 8
+    update_quality(); // + 3 = 11
+    update_quality(); // + 3 = 14
+    update_quality(); // + 3 = 17
+    update_quality(); // + 3 = 20
+    update_quality(); // 0
+    expect(items[0].quality).toBe(0);
+  });
+
+  it('"Conjured" items degrade twice as fast as normal', () => {
+    items = [
+      new Item('Conjured Mana Cake', 5, 12)
+    ];
+    update_quality(); // 10
+    update_quality(); // 8
+    update_quality(); // 6
+    expect(items[0].quality).toBe(6);
+  });
+
+  it('At the end of each day our system lowers quality for every item', () => {
+    items = [
+      new Item('foo', 3, 4),
+      new Item('bar', 3, 4)
+    ];
+    update_quality();
+    items.forEach(
+      item => {
+        expect(item.quality).toBe(3);
+      }
+    );
+  });
+
+  it('At the end of each day our system lowers sell_in for every item', () => {
+    items = [
+      new Item('foo', 3, 4),
+      new Item('bar', 3, 4)
+    ];
+    update_quality();
+    items.forEach(
+      item => {
+        expect(item.sell_in).toBe(2);
+      }
+    );
+  });
+
+  it('never modifies sulfuras which never expires and is always 80 quality', () => {
+    items = [
+      new Item(SULFURAS, 0, 80),
+    ];
+    update_quality();
+    expect(items[0].quality).toBe(80);
+    expect(items[0].sell_in).toBe(0);
+  });
 });
+
--- js/src/gilded_rose.js
+++ js/src/gilded_rose.js
@@ -6,50 +6,95 @@ function Item(name, sell_in, quality) {
 
 var items = []
 
+const DEGRADE_BASE             = 1;
+const AGED_BRIE                = 'Aged Brie';
+const BACKSTAGE_PASS           = 'BACKSTAGE_PASS';
+const CONJURED                 = 'CONJURED';
+const TAFKAL_CONCERT_BACKSTAGE = 'Backstage passes to a TAFKAL80ETC concert';
+const SULFURAS                 = 'Sulfuras, Hand of Ragnaros';
+
+/**
+ *
+ * @param {Item} item Item to degrade
+ * @param {Integer} degradeAmount amount to degrade by
+ */
+const dropQuality = (item, degradeAmount = DEGRADE_BASE) => {
+  // The quality of an item is never negative
+  item.quality = Math.max(item.quality - degradeAmount, 0);
+}
+
+/**
+ *
+ * @param {Item} item item to raise quality on
+ * @param {Integer} amount amount to raise quality by
+ */
+const raiseQuality = (item, amount) => {
+  // The quality of an item is never more than 50
+  item.quality = Math.min(item.quality + amount, 50);
+}
+
 function update_quality() {
   for (var i = 0; i < items.length; i++) {
-    if (items[i].name != 'Aged Brie' && items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-      if (items[i].quality > 0) {
-        if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-          items[i].quality = items[i].quality - 1
-        }
-      }
-    } else {
-      if (items[i].quality < 50) {
-        items[i].quality = items[i].quality + 1
-        if (items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-          if (items[i].sell_in < 11) {
-            if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1
-            }
-          }
-          if (items[i].sell_in < 6) {
-            if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1
-            }
-          }
-        }
-      }
+    const item = items[i];
+    const {name, sell_in, quality} = item;
+    // todo: talk with the Goblin because matching by name is janky
+    let category = name;
+    if(name.match(/Conjured/) !== null) {
+      category = CONJURED;
     }
-    if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-      items[i].sell_in = items[i].sell_in - 1;
+    console.log(name);
+    if(name.match(/Backstage Passes/i) !== null) {
+      category = BACKSTAGE_PASS;
     }
-    if (items[i].sell_in < 0) {
-      if (items[i].name != 'Aged Brie') {
-        if (items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-          if (items[i].quality > 0) {
-            if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-              items[i].quality = items[i].quality - 1
-            }
-          }
+    switch(category) {
+      case AGED_BRIE:
+        // "Aged Brie" actually increases in quality the older it gets
+        raiseQuality(item, 1);
+        item.sell_in--;
+        break;
+      case CONJURED:
+        // "Conjured" items degrade in quality twice as fast as normal items
+        dropQuality(item, DEGRADE_BASE * 2);
+        item.sell_in--;
+        break;
+      case BACKSTAGE_PASS:
+        console.log('backstage pass');
+        /*
+        "Backstage passes", like aged brie, increases in quality as it's sell-in
+        value approaches; quality increases by 2 when there are 10 days or less
+        and by 3 when there are 5 days or less but quality drops to 0 after the
+        concert
+        */
+        if(sell_in <= 0) {
+          // backstage passes are worthless after concert date
+          item.quality = 0;
+        } else if(sell_in <= 5) {
+          //  quality increases by 3 when there are 5 days or less
+          raiseQuality(item, 3);
+        } else if(sell_in <= 10) {
+          //  quality increases by 2 when there are 10 days or less
+          raiseQuality(item, 2);
         } else {
-          items[i].quality = items[i].quality - items[i].quality
+          // quality increases by 1 as normal
+          raiseQuality(item, 1);
         }
-      } else {
-        if (items[i].quality < 50) {
-          items[i].quality = items[i].quality + 1
+        if(sell_in > 0) {
+          item.sell_in--;
         }
-      }
+        break;
+      case SULFURAS:
+        // "Sulfuras", being a legendary item, never has to be sold or decreases in quality
+        item.sell_in = 0;
+        break;
+      default:
+        item.sell_in--;
+        dropQuality(item, (
+          (sell_in < 0)
+          // Once the sell by date has passed, quality degrades twice as fast
+          ? (DEGRADE_BASE * 2)
+          : DEGRADE_BASE
+        ));
+        break;
     }
   }
 }
